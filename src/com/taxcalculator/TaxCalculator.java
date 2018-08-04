6e6e029cde0cfb51f8825121f45cc0a59ddfca47
Sayali Martal <sayalimartal1@gmail.com>
2018-08-04 19:09:04 +0530

    .

---
+++ src/com/taxcalculator/TaxCalculator.java
@@ -0,0 +1,40 @@
+//Calculate Tax amount based on various parameters and throw necessary exceptions
+package com.taxcalculator;
+
+public class TaxCalculator {
+	
+	private String employeeName;
+	private double employeeSalary;
+	private boolean isIndian;
+	
+	public TaxCalculator(String employeeName, double employeeSalary, boolean isIndian) {
+		this.employeeName = employeeName;
+		this.employeeSalary = employeeSalary;
+		this.isIndian = isIndian;
+	}
+	
+	//Calculate tax if no exception is thrown
+	public double calculateTax() throws EmployeeNameInvalidException, TaxNotEligibleException, CountryNotValidException {
+		double taxAmount = 0;
+		if (isIndian==true) {
+			if (employeeName.isEmpty()) 
+				throw new EmployeeNameInvalidException("The employee name cannot be empty");
+			else {
+				if (employeeSalary>=1_00_000) 
+					taxAmount=employeeSalary*0.08;
+				else if(employeeSalary>=50_000) 
+					taxAmount=employeeSalary*0.06;
+				else if(employeeSalary>=30_000) 
+					taxAmount=employeeSalary*0.05;
+				else if(employeeSalary>=10_000) 
+					taxAmount=employeeSalary*0.04;
+				else 
+					throw new TaxNotEligibleException("The employee does not need to pay tax");
+			}
+		}
+		else 
+			throw new CountryNotValidException("The employee should be an Indian citizen for calculating tax.");
+		return taxAmount;
+	}
+	
+}
